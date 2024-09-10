784d5cb9927971ed696eebd9db5b78e50bd8d6f5
Sajan Poudel <sajanpoudel@MacSajan.lan>
2024-09-10 00:24:43 -0400

    Langchain Integration

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    // Exclude the Piper build directory from being processed
-    exclude: ['**/piper/**'],
-  }
-  
-  export default nextConfig;
--- src/utils/googleAuth.ts
+++
@@ -1,7 +0,0 @@
-import { google } from 'googleapis';
-
-export const createOAuth2Client = (accessToken: string) => {
-  const client = new google.auth.OAuth2();
-  client.setCredentials({ access_token: accessToken });
-  return client;
-};
